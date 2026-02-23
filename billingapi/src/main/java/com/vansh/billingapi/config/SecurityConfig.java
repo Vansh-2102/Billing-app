7fc3dda0e19527a690bdba81ac81cf1473ab27bf
Vansh-2102 <vansh250502@gmail.com>
2026-02-23 23:46:49 +0530

    Added Vercel domain to CROS

--- billingapi/src/main/java/com/vansh/billingapi/config/SecurityConfig.java
+++ billingapi/src/main/java/com/vansh/billingapi/config/SecurityConfig.java
@@ -72,8 +72,15 @@ public class SecurityConfig {
 
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
-        config.setAllowedMethods(List.of("*"));
+        config.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "https://billing-app-mu-six.vercel.app"
+        ));
+
+        config.setAllowedMethods(List.of(
+                "GET", "POST", "PUT", "DELETE", "OPTIONS"
+        ));
+
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
 
